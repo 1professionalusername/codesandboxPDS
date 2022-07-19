cface354519e815f91fa5c2c8811f246856440c3
Brandon <jailhousejordans@gmail.com>
2022-07-19 14:10:18 -0700

    Fixed Mobile view triptych Removed all but one triptych image

--- src/components/Triptych.jsx
+++ src/components/Triptych.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Triptych() {
   return (
     <div className="triptych">
-      <img src="primrose_beignet.jpg" className="cupcake" alt="cupcake" />
-      <img src="cake_slice.jpg" className="cheesecake" alt="cheesecake" />
-      <img src="primrose_doughnuts.jpg" className="lemon" alt="lemonbars" />
+      <img src="primrose_beignet.jpg" className="beignets" alt="beignets" />
+      <img src="cake_slice.jpg" className="cake_slice" alt="slice of cake" />
+      <img src="primrose_doughnuts.jpg" className="doughnuts" alt="doughnuts" />
     </div>
   );
 }
